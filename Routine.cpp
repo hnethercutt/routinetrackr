139c75f229dfcfcefe51a296aaa435c58f57306d
Hannah <144931749+conranh@users.noreply.github.com>
2025-01-09 02:33:07 -0500

    Description to string/added function to get routine steps

--- Routine.cpp
+++ Routine.cpp
@@ -8,152 +8,183 @@ using namespace std;
 
 const string daysOfWeek[] =
 {
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
 };
-
-const int MAXCHARS = 150;
 
 struct Step
 {
-    char description[MAXCHARS];
-    int frequency;
-    bool daily = true;
-    bool weekly = true;
-    int days;
+	string description;
+	int frequency;
+	bool daily = true;
+	bool weekly = true;
+	int days;
 };
 
 typedef Step ListItemType;
 
 struct Node
 {
-    ListItemType item;
-    Node* next;
+	ListItemType item;
+	Node* next;
 };
 
 struct Routine
 {
-    string title;
-    Node* steps;
+	string title;
+	Node* steps;
 };
 
 Routine* getRoutines(int& numRoutines);
+void getSteps(Routine* routines, int numRoutines);
 void newRoutine();
 
 int main()
 {
-    int numRoutines = 0;
+	int numRoutines = 0;
 
-    Routine* routines = getRoutines(numRoutines);
+	Routine* routines = getRoutines(numRoutines);
+	getSteps(routines, numRoutines);
 
-    time_t current = time(0);
-    tm local;
-    localtime_s(&local, &current);
-    int day = local.tm_wday;
+	time_t current = time(0);
+	tm local;
+	localtime_s(&local, &current);
+	int day = local.tm_wday;
 
-    int option = 0;
+	int option = 0;
 
-    while (option != 5)
-    {
-        cout << "Today's Date is "
-            << daysOfWeek[day] << ", "
-            << 1 + local.tm_mon << "/"
-            << local.tm_mday << "/"
-            << 1900 + local.tm_year << endl;
-        cout << "1. Create a New Routine" << endl;
-        cout << "2. View an Existing Routine" << endl;
-        cout << "3. Edit an Existing Routine" << endl;
-        cout << "4. View Your Stats" << endl;
-        cout << "5. Exit" << endl;
-        cout << "Choose an Option: ";
+	while (option != 5)
+	{
+		cout << "Today's Date is "
+			<< daysOfWeek[day] << ", "
+			<< 1 + local.tm_mon << "/"
+			<< local.tm_mday << "/"
+			<< 1900 + local.tm_year << endl;
+		cout << "1. Create a New Routine" << endl;
+		cout << "2. View an Existing Routine" << endl;
+		cout << "3. Edit an Existing Routine" << endl;
+		cout << "4. View Your Stats" << endl;
+		cout << "5. Exit" << endl;
+		cout << "Choose an Option: ";
 
-        cin >> option;
+		cin >> option;
 
-        switch (option)
-        {
-        case 1:
-            newRoutine();
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
-        default:
-            cout << "Invalid input\n";
-            break;
-        }
-    }
+		switch (option)
+		{
+		case 1:
+			newRoutine();
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		default:
+			cout << "Invalid input\n";
+			break;
+		}
+	}
 
-    return 0;
+	return 0;
 }
 
 Routine* getRoutines(int& numRoutines)
 {
-    ifstream inFile("Routines.txt");
+	ifstream inFile("Routines.txt");
 
-    if (!inFile)
-    {
-        cerr << "ERROR: File cannot be opened.";
-        exit(1);
-    }
+	if (!inFile)
+	{
+		cerr << "ERROR: File cannot be opened.";
+		exit(1);
+	}
 
-    string line;
+	string line;
 
-    while (getline(inFile, line))
-    {
-        numRoutines++;
-    }
+	while (getline(inFile, line))
+	{
+		numRoutines++;
+	}
 
-    inFile.clear();
-    inFile.seekg(0, ios::beg);
+	inFile.clear();
+	inFile.seekg(0, ios::beg);
 
-    Routine* routines = new Routine[numRoutines];
+	Routine* routines = new Routine[numRoutines];
 
-    for (int i = 0; getline(inFile, line); i++)
-    {
-        routines[i].title = line;
-    }
+	for (int i = 0; getline(inFile, line); i++)
+	{
+		routines[i].title = line;
+	}
 
-    inFile.close();
+	inFile.close();
 
-    return routines;
+	return routines;
+}
+
+void getSteps(Routine* routines, int numRoutines)
+{
+	ifstream inFile("Steps.txt");
+
+	if (!inFile)
+	{
+		cerr << "ERROR: File cannot be opened.";
+		exit(1);
+	}
+
+	for (int i = 0; i < numRoutines; i++)
+	{
+		string description;
+
+		while (getline(inFile, description) && description != "*")
+		{
+			Node* newNode = new Node;
+
+			inFile >> newNode->item.frequency
+				>> newNode->item.daily
+				>> newNode->item.weekly
+				>> newNode->item.days;
+			inFile.ignore(numeric_limits<streamsize>::max(), '\n');
+
+			newNode->item.description = description;
+			newNode->next = routines[i].steps;
+			routines[i].steps = newNode;
+		}
+	}
 }
 
 void newRoutine()
 {
-    ofstream outFile("Routines.txt", ios::app);
+	ofstream outFile("Routines.txt", ios::app);
 
-    if (!outFile)
-    {
-        cerr << "ERROR: File cannot be opened.";
-    }
+	if (!outFile)
+	{
+		cerr << "ERROR: File cannot be opened.";
+	}
 
-    string name;
-    char choice;
-    bool newRoutine = true;
-    while (newRoutine)
+	string name;
+	char choice;
+	bool newRoutine = true;
+	while (newRoutine)
 
-    {
-        cout << "Please enter a name for your new routine: ";
-        cin >> name;
+	{
+		cout << "Please enter a name for your new routine: ";
+		cin >> name;
 
-        outFile << name << "\n";
+		outFile << name << "\n";
 
-        cout << "Create another routine? Enter Y for Yes or N for No: ";
-        cin >> choice;
+		cout << "Create another routine? Enter Y for Yes or N for No: ";
+		cin >> choice;
 
-        if (choice == 'N' || choice == 'n')
-        {
-            newRoutine = false;
-        }
-    }
+		if (choice == 'N' || choice == 'n')
+		{
+			newRoutine = false;
+		}
+	}
 }
